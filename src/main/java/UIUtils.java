18f84e0101103233130185fc6b03c2a0a5162a36
Payton <plowry@bsu.edu>
2020-04-08 12:56:00 -0400

    Made Pie Chart work.

--- src/main/java/UIUtils.java
+++ src/main/java/UIUtils.java
@@ -7,8 +7,11 @@ import java.util.List;
 public class UIUtils {
 
     public static PieDataset convertPairsToPieDataset(List<Pair<String, Integer>> pairs) {
-        // TODO
-        return null;
+        DefaultPieDataset results = new DefaultPieDataset();
+        results.setValue(pairs.get(0).getValue0(), pairs.get(0).getValue1());
+        results.setValue(pairs.get(1).getValue0(), pairs.get(1).getValue1());
+        results.setValue(pairs.get(2).getValue0(), pairs.get(2).getValue1());
+        return results;
     }
 
 }
