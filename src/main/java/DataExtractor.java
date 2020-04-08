b16575b7bb5ba3424128b6f3b81c952bb9eebd08
Dot Horsman <dottyhorsman@bsu.edu>
2020-04-08 14:23:01 -0400

    Added getRawJSONfromAPI, extractDataFromJSON and convertPairsToPieDataset

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -5,12 +5,26 @@ import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
-public class DataExtractor {
+public class DataExtractor
+{
+    public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson)
+    {
+        ArrayList<Pair<String, Integer>> returnList = new ArrayList <Pair<String, Integer>> ();
 
-    public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson) {
-        // TODO
-        return null;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(rawJson);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+
+        Pair newestPair;
+        for (Map.Entry<String, JsonElement> entry : rootObject.entrySet())
+        {
+            newestPair = new Pair<String,Integer>(entry.getKey(),
+                    entry.getValue().getAsInt());
+            returnList.add(newestPair);
+        }
+
+        return returnList;
     }
-
 }
--- src/main/java/DataFetcher.java
+++ src/main/java/DataFetcher.java
@@ -5,11 +5,14 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 
 import java.io.IOException;
 
-public class DataFetcher {
-
-    public static String getRawJSONfromAPI() throws Exception {
-        // TODO
-        return null;
+public class DataFetcher
+{
+    public static String getRawJSONfromAPI() throws Exception
+    {
+        HttpRequestFactory requestFactory;
+        requestFactory = new NetHttpTransport().createRequestFactory();
+        HttpRequest getRequest = requestFactory.buildGetRequest(new GenericUrl("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief"));
+        String rawResponse = getRequest.execute().parseAsString();
+        return rawResponse;
     }
-
 }
