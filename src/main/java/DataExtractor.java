c51b4f5a8e1d17419c401f1e1fa477884371acbc
Nick <nbash@bsu.edu>
2020-04-08 13:50:33 -0400

    HelloPieChart In-Class Work

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -5,12 +5,22 @@ import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DataExtractor {
 
     public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson) {
-        // TODO
-        return null;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(rawJson);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        List<Pair<String, Integer>> pairList = new ArrayList<>();
+        for (Map.Entry<String, JsonElement> entry : rootObject.entrySet()) {
+            var someString = entry.getKey();
+            var someInt = entry.getValue().getAsInt();
+            pairList.add(new Pair<String, Integer>(someString, someInt));
+        }
+        return pairList;
     }
 
 }
--- src/main/java/DataFetcher.java
+++ src/main/java/DataFetcher.java
@@ -3,13 +3,15 @@ import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.javanet.NetHttpTransport;
 
-import java.io.IOException;
-
 public class DataFetcher {
 
     public static String getRawJSONfromAPI() throws Exception {
-        // TODO
-        return null;
+        HttpRequestFactory requestFactory;
+        requestFactory = new NetHttpTransport().createRequestFactory();
+        HttpRequest getRequest = requestFactory.buildGetRequest (
+                new GenericUrl("https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/brief"));
+        String rawResponse = getRequest.execute().parseAsString();
+        return rawResponse;
     }
 
 }
