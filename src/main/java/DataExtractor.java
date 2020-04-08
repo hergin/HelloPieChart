b31298d557e09addb2d20d8e1c48b05cbf9e3ff6
Jacob Deffendall <jddeffendall@bsu.edu>
2020-04-08 12:55:48 -0400

    Added methods

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -1,3 +1,4 @@
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -5,12 +6,25 @@ import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class DataExtractor {
 
     public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson) {
-        // TODO
-        return null;
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(rawJson);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        // rootObject.entrySet() will give you a list of Key-Value for the data in JSON.
+        // Basically, go over each entry in a for loop and add a Pair to a resulting list for each.
+
+        List<Pair<String, Integer>> resultList = new ArrayList<>();
+
+        for (Map.Entry<String, JsonElement> e : rootObject.entrySet()) {
+            resultList.add(new Pair<String, Integer>(e.getKey(), e.getValue().getAsInt()));
+        }
+
+        return resultList;
     }
 
 }
