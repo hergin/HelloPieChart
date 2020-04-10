0d179d4969b7add4a91dd17d10b821707e9179bc
Luke Holliday <lmholliday@bsu.edu>
2020-04-10 16:54:40 -0400

    CS222 HelloPiechart finished

--- src/main/java/CoronaUI.java
+++ src/main/java/CoronaUI.java
@@ -55,3 +55,4 @@ public class CoronaUI extends JFrame {
     }
 
 }
+
--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -3,14 +3,23 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.javatuples.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class DataExtractor {
 
     public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson) {
         // TODO
-        return null;
+        List<Pair<String, Integer>> theList = new LinkedList<>();
+        JsonParser jsonParser = new JsonParser();
+        JsonElement rootElement = jsonParser.parse(rawJson);
+        JsonObject rootObject = rootElement.getAsJsonObject();
+        Set<Map.Entry<String, JsonElement>> es = rootObject.entrySet();
+        for (Map.Entry<String, JsonElement> e : es){
+            String theKey = e.getKey();
+            int theValue = e.getValue().getAsInt();
+            theList.add(new Pair<String, Integer>(theKey, theValue));
+        }
+        return theList;
     }
 
 }
