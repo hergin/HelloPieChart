ba0d605f91634b3d47f834681d80a967f6fae127
Eric Vester <ericevester@gmail.com>
2020-04-11 18:01:45 -0400

    Modified DataExtractor, DataFetcher, and UIUtils to work.

--- src/main/java/DataExtractor.java
+++ src/main/java/DataExtractor.java
@@ -5,12 +5,22 @@ import org.javatuples.Pair;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class DataExtractor {
 
-    public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson) {
-        // TODO
-        return null;
+    public static List<Pair<String, Integer>> extractDataFromJSON(String rawJson){
+    JsonParser jsonParser = new JsonParser();
+    JsonElement rootElement = jsonParser.parse(rawJson);
+    JsonObject rootObject = rootElement.getAsJsonObject();
+    List<Pair<String,Integer>> list = new ArrayList<>();
+        for (Map.Entry<String, JsonElement> entry:rootObject.entrySet()) {
+        String key = entry.getKey();
+        Integer integer = entry.getValue().getAsInt();
+        Pair<String, Integer> pair = new Pair<String, Integer>(key , integer);
+        list.add(pair);
     }
+        return list;
+}
 
 }
